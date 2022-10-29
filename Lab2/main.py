f2e2f4fd9130cda5d6361b8c630aef3d1ae74f6b
Arhastran <63006166+Arhastran@users.noreply.github.com>
2022-10-29 16:00:27 +0200

    Changes (plot)

--- Lab2/main.py
+++ Lab2/main.py
@@ -5,6 +5,7 @@
 import numpy as np
 import random as random
 import tqdm
+import matplotlib.pyplot as plt
 
 class IsingModel:
     def __init__(self , spin = 0, M = 1, N=1 , B = 1, Beta= 1, J = 1):
@@ -13,13 +14,13 @@ class IsingModel:
 
     def randomspins(self, M, N):  # random spins web generator
         spinslist = np.zeros((M,N), dtype = int)
-        for j in range(M):
-            for i in range(N):
+        for i in range(M):
+            for j in range(N):
                 self.spin = random.randint(0, 1)
                 if self.spin == 0:
-                    spinslist[i,j] = -1
+                    spinslist[i , j] = -1
                 else:
-                    spinslist[i, j] = 1
+                    spinslist[i , j] = 1
         return (spinslist)
 
     def hamiltonian(self, J,spins):
@@ -38,16 +39,19 @@ class IsingModel:
 B = 1
 Beta = 0.00001
 J = 0.35
-M = 10
+M = 20
 N = 10
 steps = 1
+
+stepsforplot = []
+spinsforplot = []
 
 a = IsingModel()
 aha = a.randomspins(M,N)
 
 
-for i in tqdm.tqdm(range(len(aha))):
-    for j in range(len(aha)):
+for i in tqdm.tqdm(range(M)):
+    for j in range(N):
         E0 = a.hamiltonian(J,aha) - B*aha[i,j]
         aha[i,j] = -aha[i,j]
         E1 = a.hamiltonian(J,aha) - B*aha[i,j]
@@ -59,9 +63,13 @@ for i in tqdm.tqdm(range(len(aha))):
             aha[i, j] = -aha[i, j]
         else:
             pass
+        spinsforplot.append(np.sum(aha)/(M*N))
 
-
-
+stepsforplot = range(0,M*N)
+plt.plot(stepsforplot,spinsforplot)
+plt.xlabel("Step[n.o]")
+plt.ylabel("Sum[a.u.]")
+plt.show()
 
 
 
