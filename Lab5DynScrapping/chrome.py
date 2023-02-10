b09577cb4d43ad40288c1668dde8b259a412e237
Arhastran <63006166+Arhastran@users.noreply.github.com>
2023-02-10 11:24:16 +0100

    muj borze to tez zaczęło dzialac haha xd

--- Lab5DynScrapping/chrome.py
+++ Lab5DynScrapping/chrome.py
@@ -4,71 +4,73 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import time
+from selenium.common.exceptions import NoSuchElementException
+from rich import print as rprint
+from rich.console import Console
 
-def scrape_movies(driver):
-    # Create an instance of ChromeOptions
+def scrape_movies(driver, search):
+
     options = Options()
-    # Add the --disable-cookies argument
+
     options.add_argument("--disable-cookies")
-    # Start the Chrome browser
+
     driver = webdriver.Chrome(options=options)
-    # Navigate to the YouTube main page
-    driver.get("https://www.youtube.com")
+
+    driver.get('https://www.youtube.com')
     driver.maximize_window()
-    driver.find_element(By.XPATH, '//*[@id="uc-btn-accept-banner"]').click()
+    time.sleep(10)
+    driver.find_element(By.XPATH,'//*[@id="content"]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button').click()
+    time.sleep(5)
+
+    #time.sleep(10)
+    #driver.find_element(By.NAME,"agreeButton").click()
+    #driver.find_element(By.CSS_SELECTOR("agreeButton")).click()
+    #try:
+    #    frame = driver.find_element(By.XPATH,'//*[@id="cnsw"]/iframe')  # <-locating chrome cookies consent frame
+    #    driver.switch_to.frame(frame)
+    #    driver.find_element(By.XPATH,'//*[@id="content"]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button').click()
+    #except NoSuchElementException:
+    #    driver.find_element(By.XPATH, '//*[@id="L2AGLb"]').click()  # <- pay attention to new id.
+    #driver.find_element(By.XPATH, '//*[@id="uc-btn-accept-banner"]').click()
+    time.sleep(10)
   #Scroll through the page
     SCROLL_PAUSE_TIME = 2
     # Get the list of all open windows
 
-    last_height = driver.execute_script("return document.body.scrollHeight")
+    #last_height = driver.execute_script("return document.body.scrollHeight")
     time.sleep(5)
     search_bar = driver.find_element(By.NAME,'search_query')
     # Click on the search bar
     search_bar.click()
-    search_bar.send_keys("Warhammer 40k")
+    search_bar.send_keys(search)
     search_bar.submit()
-
-    while True:
-        # Scroll down to the bottom of the page
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        # Wait for the page to load
-        wait = WebDriverWait(driver, SCROLL_PAUSE_TIME)
-        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".ytd-watch-next-secondary-results-renderer")))
-
-        # Calculate the new height of the page
-        new_height = driver.execute_script("return document.body.scrollHeight")
-
-        # If the new height is the same as the last height, it means that the page has finished loading
-        if new_height == last_height:
-            break
-
-        # Update the last height
-        last_height = new_height
-
-
+    movies = []
+    for i in range(2):
+        driver.execute_script("window.scrollBy(0, window.innerHeight);")
+        time.sleep(2)
+        titles = driver.find_elements(By.XPATH, '//*[@id="video-title"]')
+        movies.append([title.text for title in titles])
     #wait = WebDriverWait(driver, 10)
-    titles = driver.find_elements(By.XPATH, '//*[@id="video-title"]')
 
-    # Extract the movie titles
-    movies = [title.text for title in titles]
 
-    # Return the movie titles
+   # movies = [title.text for title in titles]
+
     return movies
 
 
-# Start the Chrome browser
+
 driver = webdriver.Chrome()
 time.sleep(5)
-# Call the scrape_movies function
 
-movies = scrape_movies(driver)
+search = "Fiber Bragg Grating"
+movies = scrape_movies(driver,search)
 
-# Print the movie titles
-print(movies)
+#print(movies)
 
-# Close the browser
 driver.quit()
+
+console = Console()
+rprint(movies)
 
 
 
