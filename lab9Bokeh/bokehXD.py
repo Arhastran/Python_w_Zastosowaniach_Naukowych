f8793d3c539d8d0e3554d043e2720a9beca57be4
Arhastran <63006166+Arhastran@users.noreply.github.com>
2023-02-10 09:37:08 +0100

    działa HAHAHAHHAHAHAH

--- lab9Bokeh/bokehXD.py
+++ lab9Bokeh/bokehXD.py
@@ -4,6 +4,12 @@ import numpy as np
 from scipy.integrate import odeint
 from bokeh.plotting import ColumnDataSource
 from bokeh.layouts import column, row
+from bokeh.io import curdoc
+import pandas as pd
+
+
+#COMMANDS:
+#  bokeh serve --show bokehXD.py
 
 def SIR(y,t,N,beta,gamma):
   S,I,R = y
@@ -12,7 +18,7 @@ def SIR(y,t,N,beta,gamma):
   dR = gamma*I
   return dS,dI,dR
 
-t = np.linspace(0,60,100)
+t = np.linspace(0,50,100)
 array_length = len(t)
 last_element = t[array_length - 1]
 N = 10000
@@ -26,75 +32,69 @@ gamma=0.25
 t = np.linspace(0,60,100)
 
 sol = odeint(SIR,y_0,t,(N,beta,gamma))
+Results = pd.DataFrame({"t" : t, "S" : list(sol[:,0]), "I" : list(sol[:,1]), "R" : list(sol[:,2])})
+source = ColumnDataSource(Results)
 
 #p = figure(x_range=(1,9), width=500, height=250)
 slider_beta = Slider(start =0, end = 1, step = 0.05, value=0.5, title='Beta')
 slider_gamma = Slider(start =0, end = 1, step = 0.05, value=0.25, title='Gamma')
-
-
+slider_N = Slider(start =0, end = 10000, step = 10, value=10000, title='N')
+#slider_S_0 = Slider(start =0, end = 10000, step = 10, value=8900, title='S_0')
 
 array_lengthy = len(sol)
 last_elementy = sol[array_lengthy - 1]
 
-
 fig = figure()
-plot = figure(x_range=(0, 1000), y_range=(0, 1000), width=350, height=350)
-res = list(sol[:,0])
-res2 = list(sol[:,1])
-res3 = list(sol[:,2])
-data1 = dict(x=t,y=res)
-data2 = dict(x=t,y=res2)
-data3 = dict(x=t,y=res3)
-print(res)
-source1 = ColumnDataSource(data=data1)
-source2 = ColumnDataSource(data=data2)
-source3 = ColumnDataSource(data=data3)
 
+#res = list(sol[:,0])
+#res2 = list(sol[:,1])
+#res3 = list(sol[:,2])
+#data1 = dict(x=t,y=res)
+#data2 = dict(x=t,y=res2)
+#data3 = dict(x=t,y=res3)
+
+fig.line("t","S", color = "red", source=source)
+fig.line("t","I", color = "green", source=source)
+fig.line("t","R", color = "blue",  source=source)
 
 def Update_beta(attr, old, new):
     global beta
     beta = new
-    sol = odeint(SIR, y_0, t, (new, gamma))
-    new_data1 = dict(x=t,y=list(sol[:,0]))
-    new_data2 = dict(x=t, y=list(sol[:, 1]))
-    new_data3 = dict(x=t, y=list(sol[:, 2]))
-    source1.data = new_data1
-    source2.data = new_data2
-    source3.data = new_data3
+    sol = odeint(SIR, y_0, t, (N, beta, gamma))
+    new_results = pd.DataFrame({"t": t, "S": list(sol[:, 0]), "I": list(sol[:, 1]), "R": list(sol[:, 2])})
+    source.data = ColumnDataSource.from_df(new_results)
 
 def Update_gamma(attr,old,new):
     global gamma
     gamma = new
-    sol = odeint(SIR, y_0, t, (beta, new))
-    new_data1 = dict(x=t, y=list(sol[:, 0]))
-    new_data2 = dict(x=t, y=list(sol[:, 1]))
-    new_data3 = dict(x=t, y=list(sol[:, 2]))
-    source1.data = new_data1
-    source2.data = new_data2
-    source3.data = new_data3
+    sol = odeint(SIR, y_0, t, (N, beta, gamma))
+    new_results = pd.DataFrame({"t": t, "S": list(sol[:, 0]), "I": list(sol[:, 1]), "R": list(sol[:, 2])})
+    source.data = ColumnDataSource.from_df(new_results)
 
+def Update_N(attr,old,new):
+    global N
+    N = new
+    sol = odeint(SIR, y_0, t, (N, beta, gamma))
+    new_results = pd.DataFrame({"t": t, "S": list(sol[:, 0]), "I": list(sol[:, 1]), "R": list(sol[:, 2])})
+    source.data = ColumnDataSource.from_df(new_results)
 
-plot.line(color = "red",legend_label="S", line_width=2,source=source1)
-plot.line(color = "green",legend_label="I", line_width=2,source=source2)
-plot.line(color = "blue",legend_label="R", line_width=2,source=source3)
 
 slider_beta.on_change("value",Update_beta)
 slider_gamma.on_change("value",Update_gamma)
-
-layout = row(
-    plot,
-    column(slider_beta,slider_gamma),
-)
+slider_N.on_change("value",Update_N)
 
 
+#layout = row(
+#    plot,
+#    column(slider_beta,slider_gamma),
+#)
 
-show(layout)
+#print(source1.data)
 
-#doc.add_root(column(slider_beta,slider_gamma, plot))
+#show(layout)
 
-# creating the application and running the server local, (http://localhost:5000), port 5000 can be changed
-#apps = {'/': Application(FunctionHandler(make_page))}
-#server = Server(apps, port=5000)
-#server.start()
+curdoc().add_root(row(column(Div(text = "<b>Model SIR</b>", align = "center"),slider_gamma,slider_beta,slider_N),fig))
+
+
 
 
